5130746224398c178e4cd09feca5eac4d90bef99
faizan <faizan.ah862@gmail.com>
2024-07-10 19:53:27 +0300

    Add exception handling

--- src/RandomNumber/Generator.java
+++ src/RandomNumber/Generator.java
@@ -11,25 +11,31 @@ public class Generator {
         int max = 100;
         int randomNumber = rand.nextInt(max) + min;
         while (true) {
-            System.out.println("Guess a number between 1 and 100");
-            int guess = Integer.parseInt(scanner.nextLine());
-            if (guess == randomNumber) {
-                System.out.println("LETS GOO! You got it right!");
-                while (true) {
-                    System.out.print("Do you want to continue playing? (y/n): ");
-                    String continueInput = scanner.nextLine();
-                    if (continueInput.equalsIgnoreCase("y")) {
-                        break;
-                    } else if (continueInput.equalsIgnoreCase("n")) {
-                        return;
-                    } else {
-                        System.out.println("Invalid input. Please enter 'y' or 'n'.");
+            try {
+                System.out.println("Guess a number between 1 and 100");
+                int guess = Integer.parseInt(scanner.nextLine());
+                if (guess > 100 || guess < 1) {
+                    System.out.println("Guess should be between 1 and 100");
+                } else if (guess == randomNumber) {
+                    System.out.println("LETS GOO! You got it right!");
+                    while (true) {
+                        System.out.print("Do you want to continue playing? (y/n): ");
+                        String continueInput = scanner.nextLine();
+                        if (continueInput.equalsIgnoreCase("y")) {
+                            break;
+                        } else if (continueInput.equalsIgnoreCase("n")) {
+                            return;
+                        } else {
+                            System.out.println("Invalid input. Please enter 'y' or 'n'.");
+                        }
                     }
+                } else if (guess > randomNumber) {
+                    System.out.println("Your guess is a bit high, go lower!");
+                } else {
+                    System.out.println("Your guess is a bit lower, go higher!");
                 }
-            } else if(guess > randomNumber){
-                System.out.println("Your guess is a bit high, go lower!");
-            } else {
-                System.out.println("Your guess is a bit lower, go higher!");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input. Type a number!");
             }
         }
     }
